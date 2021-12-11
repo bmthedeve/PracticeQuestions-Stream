36e2862de1483185385c84e86b1ae53851b71b06
Balamurugan <95612809+bmthedeve@users.noreply.github.com>
2021-12-11 19:36:07 +0530

    Update InsuranceApp.java

--- InsuranceApp.java
+++ InsuranceApp.java
@@ -2,27 +2,22 @@ package PracticeQuestions;
 import java.util.*;
 import java.util.ArrayList;
 public class InsuranceApp {
-
+	
+	static List<Insurance>l = new ArrayList<Insurance>();
+	
 	public static void main(String[] args) {
-		Insurance i1=new Insurance("Health","JeevanTharan",17000);
-		Insurance i2=new Insurance("Car","safeCar",13000);
-		Insurance i3=new Insurance("Life","lifeInsur",14000);
-		Insurance i4=new Insurance("Pet","petInsur",15000);
-		Insurance i5=new Insurance("Health","LongLive",16000);
 		
-		ArrayList<Insurance>l = new ArrayList<Insurance>();
-//		List<Insurance>l = Arrays.asList(i1,i2,i3,i4,i5);
-		l.add(i1);
-		l.add(i2);
-		l.add(i3);
-		l.add(i4);
-		l.add(i5);
-//		System.out.println(l);
+		l.add(new Insurance("Health","JeevanTharan",17000));
+		l.add(new Insurance("Car","safeCar",13000));
+		l.add(new Insurance("Life","lifeInsur",14000));
+		l.add(new Insurance("Pet","petInsur",15000));
+		l.add(new Insurance("Health","LongLive",16000));
+
+		ArrayList<Insurance> arl = new ArrayList<>(l);
 		
-//		public static Integer totalPremium(ArrayList<Insurance> l) {
-//			
-		ArrayList<Insurance> li = InsuranceAnalysis.getInsuranceDetailsByCostAscending(l);
-		System.out.println(li);
+		System.out.println(InsuranceAnalysis.getInsuranceDetailsByCostAscending(arl));
+		System.out.println(InsuranceAnalysis.totalPremiumCost(arl));
+		
 	}
 
 }
