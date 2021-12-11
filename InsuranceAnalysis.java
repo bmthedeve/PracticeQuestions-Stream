f917b3dac41da88c5342df0247a224b5749160eb
Balamurugan <95612809+bmthedeve@users.noreply.github.com>
2021-12-11 19:36:46 +0530

    Update InsuranceAnalysis.java

--- InsuranceAnalysis.java
+++ InsuranceAnalysis.java
@@ -1,11 +1,16 @@
+//For defining methods
+
 package PracticeQuestions;
 import java.util.*;
 import java.util.stream.Collectors;
 public class InsuranceAnalysis {
-//	public int totalPremiumCost(ArrayList<Insurance> insurances) {
-//		int sum = 0;
-//	return insurances.stream().filter(o->o.getPremiumPerYear()>10000).map(o->o.getPremiumPerYear()).forEach(o->sum+=o);
-//	}
+	//If the premium per year is >10000, add it in the sum
+	//If the premium per year is <10000, don't add it in the sum
+	//If all the premium per year is <10000 return zero
+	public static int totalPremiumCost(ArrayList<Insurance> insurances) {
+		int sum = insurances.stream().filter(o->o.getPremiumPerYear()>10000).collect(Collectors.summingInt(Insurance::getPremiumPerYear));
+		return sum;
+	}
 	
 	public static ArrayList<Insurance> getInsuranceDetailsByCostAscending(ArrayList<Insurance> l){
 		List<Insurance> li = l.stream().sorted((o1,o2)->o1.getPremiumPerYear()-o2.getPremiumPerYear()).collect(Collectors.toList());
